dbfe8faa5879239f18ffd8e936697cc0242299d2
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-07-29 15:47:42 +0300

    feat: add redirect handler

--- cmd/url-shorter/main.go
+++ cmd/url-shorter/main.go
@@ -58,6 +58,7 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
 	log.Info("starting server", slog.String("address", cfg.Address))
 
 	srv := &http.Server{
